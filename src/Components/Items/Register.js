3627a15e0f2176d71dd32a39aa1f83ea6a899b7c
Elad Zucker <elad-zucker@abra-it.com>
2022-06-15 16:30:57 +0300

    .

--- src/Components/Items/Register.js
+++ src/Components/Items/Register.js
@@ -1,42 +1,57 @@
 import { useRef } from "react";
+import { useForm } from "react-hook-form";
 
 import { registerUser } from "../../Services/Api";
 
+/*
+ Homework :
+    Use react-hook-forms ( https://react-hook-form.com/ ) to validate all data of the register
+    form.
+
+    use documentation to understand how to use (hint. use useForm ).
+    make realtime validation and show an error if any of the inputs contains errors.
+
+*/
 const Register = () => {
-    console.log("In render");
+    const { register, handleSubmit, formState: { errors, isValid } } = useForm({mode: "onChange"});
+    console.log(isValid);
+    const Submit = async (data) => {
+        console.log(data);
+        // try {
+        //     const data = await registerUser(
+        //         username.current.value, 
+        //         password.current.value, 
+        //         email.current.value, 
+        //         firstName.current.value, 
+        //         lastName.current.value);
 
-    const username = useRef("");
-    const password = useRef("");
-    const email = useRef("");
-    const firstName = useRef("");
-    const lastName = useRef("");
+        //     alert("Register user successfully.");
 
-    const Submit = async (event) => {
-        event.preventDefault();
-        try {
-            const data = await registerUser(
-                username.current.value, 
-                password.current.value, 
-                email.current.value, 
-                firstName.current.value, 
-                lastName.current.value);
-
-            alert("Register user successfully.");
-
-        } catch (err) {
-            alert(err);
-        }
+        // } catch (err) {
+        //     alert(err);
+        // }
     }
-    
+
     return (
         <>
             <form>
-                <label className="labels">Username :</label><input ref={username} type="text"></input><br />
-                <label className="labels">Email :</label><input ref={email} type="email"></input><br />
-                <label className="labels">Password :</label><input ref={password} type="password"></input><br />
-                <label className="labels">Firstname :</label><input ref={firstName} type="text"></input><br />
-                <label className="labels">LastName :</label><input ref={lastName} type="text"></input><br />
-                <button onClick={Submit}>Submit</button>
+                <label className="labels">Username :</label>
+                <input 
+                    {...register("username", 
+                        { required: "the username is required."})} 
+                        type="text">
+                </input><br />
+                
+                { errors.username && <p>{errors.username.message} </p> }
+                <label className="labels">Email :</label><input {...register("email", { required:  "the email is required."})} type="email"></input><br />
+                { errors.email && <p>{errors.email.message} </p> }
+                <label className="labels">Password :</label><input {...register("password", { required:  "the password is required."})} type="password"></input><br />
+                { errors.password && <p>{errors.password.message} </p> }
+                <label className="labels">Firstname :</label><input {...register("firstname", { required: "the firstname is required."})} type="text"></input><br />
+                { errors.firstname && <p>{errors.firstname.message} </p> }
+                <label className="labels">LastName :</label><input {...register("lastname", { required: "the lastname is required."})} type="text"></input><br />
+                { errors.lastname && <p>{errors.lastname.message} </p> }
+                <button disabled={!isValid} onClick={handleSubmit(Submit)}>Submit</button>
             </form>
         </>
     )
